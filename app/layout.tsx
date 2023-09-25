7bd5040c982c53bfdeba04c166b2e0bdada163de
appsius <ilyas.ustunkaya234434@gmail.com>
2023-09-25 09:17:28 +0300

    Improve metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,14 @@ export const metadata: Metadata = {
   description:
     'An nextjs app that applied the website page optimization perfectly.',
   icons: [{ rel: 'icon', url: 'favicon.ico' }],
+  other: {
+    'theme-color': '#0d1117',
+    'color-scheme': 'dark only',
+    'twitter:image': 'https://ibb.co/G7HsB9d', // share image
+    'twitter:card': 'summary_large_image',
+    'og:url': 'https://ibb.co/G7HsB9d',
+    'og:type': 'website',
+  },
 };
 
 export default function RootLayout({
