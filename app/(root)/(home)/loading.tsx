073a5d4b0a246e6e82e841ca5657b55010933496
appsius <ilyas.ustunkaya234434@gmail.com>
2023-09-25 08:47:48 +0300

    Fix skeleton bg-color

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -4,15 +4,15 @@ const Loading = () => {
   return (
     <main className='flex-center flex-col paddings mx-auto w-full max-w-screen-2xl'>
       <section className='nav-padding w-full'>
-        <Skeleton className='bg-red-600 h-[274px] w-full rounded-lg' />
+        <Skeleton className='bg-black-200/40 h-[274px] w-full rounded-lg' />
       </section>
 
       <section className='flex flex-col mt-6 w-full sm:mt-20'>
-        <Skeleton className='bg-red-600 h-10 w-56' />
+        <Skeleton className='bg-black-200/40 h-10 w-56' />
         <div className='flex flex-wrap justify-center mt-12 w-full gap-16 sm:justify-start'>
-          <Skeleton className='bg-red-600 h-[440px] w-full sm:h-[356px]' />
-          <Skeleton className='bg-red-600 h-[440px] w-full sm:h-[356px]' />
-          <Skeleton className='bg-red-600 h-[440px] w-full sm:h-[356px]' />
+          <Skeleton className='bg-black-200/40 h-[440px] w-full sm:h-[356px]' />
+          <Skeleton className='bg-black-200/40 h-[440px] w-full sm:h-[356px]' />
+          <Skeleton className='bg-black-200/40 h-[440px] w-full sm:h-[356px]' />
         </div>
       </section>
     </main>
